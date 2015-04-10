dde13f0a5f0360ece663443516f526c50495a34c
Roman Storm <rstormsf@gmail.com>
2015-04-10 15:55:46 -0700

    updating record

--- test.py
+++ test.py
@@ -2,9 +2,14 @@ import sys
 sys.path.append("mongo-java-driver-3.0.0-rc1.jar")
 
 from com.mongodb import *
+from com.mongodb.client.model import *
+from org.bson import *
 
 m = MongoClient("localhost")
-db = m.getDB("test")
-coll = db.getCollection("accounts")
-a = coll.findOne()
-print a
+db = m.getDatabase("test")
+coll = db.getCollection("dub_accs")
+print coll.count()
+user = coll.find(Filters.and(Filters.exists("ImageUrl"), Filters.exists("snapshot", False))).first()
+email = user["EmailAddress"]
+coll.updateOne(Filters.eq("EmailAddress", email), Document("$set", Document("snapshot", True)))
+
